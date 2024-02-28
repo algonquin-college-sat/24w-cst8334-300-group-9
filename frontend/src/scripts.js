782ae8cafa4229b27df2f3ebbcdf97c93681322b
vtran <vytran1805@gmail.com>
2024-02-28 02:53:06 -0500

    Finish frontend structure

---
+++ frontend/src/scripts.js
@@ -0,0 +1,5 @@
+import './components/popup/index.js';
+import './pages/overview/index.js';
+import './pages/boardSelection/index.js';
+import './tickets/celebration/index.js';
+import './tickets/improvement/index.js';
