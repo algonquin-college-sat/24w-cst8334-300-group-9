470e4e893fca4fee58bb458dc74ba1771cf69173
vtran <vytran1805@gmail.com>
2024-03-07 11:01:10 -0500

    Create board selection

--- frontend/src/scripts.js
+++ frontend/src/scripts.js
@@ -1,5 +1,17 @@
-import './components/popup/index.js';
-import './pages/overview/index.js';
-import './pages/boardSelection/index.js';
-import './tickets/celebration/index.js';
-import './tickets/improvement/index.js';
+// import './components/popup/index.js';
+// import './pages/overview/index.js';
+// import './pages/boardSelection/index.js';
+// import './tickets/celebration/index.js';
+// import './tickets/improvement/index.js';
+
+const dashboard1 = document.querySelector('.dashboard-1');
+const dashboard2 = document.querySelector('.dashboard-2');
+const dashboard3 = document.querySelector('.dashboard-3');
+console.log('inside js');
+const navigateToDashBoard = (url) => {
+  window.location.href = url;
+};
+dashboard1.addEventListener('click', () => {
+  navigateToDashBoard('../pages/overview/index.js');
+  console.log('clicked!');
+});
