8478219a8d8fa3e5d89a17404f392b0d9b06b335
Gillian Webster <webs0108@algonquinlive.com>
2024-03-11 12:58:53 -0400

    adding untracked files

---
+++ frontend/src/pages/overview/overview.js
@@ -0,0 +1,40 @@
+// index.js文件内容
+function openModal() {
+    document.getElementById("ticketModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("ticketModal").style.display = "none";
+  }
+  
+  function addImprovement() {
+    console.log("Adding an Improvement Ticket...");
+    closeModal();
+  }
+  
+  function addCelebration() {
+    console.log("Adding a Celebration Ticket...");
+    closeModal();
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.button').addEventListener('click', openModal);
+    document.querySelector('#demoTicket').addEventListener('click', viewImprovement);
+});
+
+// Jeremy Wang adds a function on 2024-03-09
+function addImprovement() {
+  // Redirects to the Improvement Ticket page
+  window.location.href = "../../tickets/improvement/add-improvement.html";
+}
+
+//Similar to the Improvement Ticket page, it will redirect the user to the Celebration Ticket page
+function addCelebration() {
+  window.location.href = "../../tickets/celebration/index.html";
+}
+
+//For demo 2
+function viewImprovement(){
+  window.location.href="../../tickets/improvement/filled-ticket.html";
+}
+
