dba6321b7f5d17296edd7486bcd820db3b2c4904
vtran <vytran1805@gmail.com>
2024-04-15 11:45:34 -0400

    Modify dbConfig, db credentials

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -4,7 +4,7 @@ import sql from 'mssql/msnodesqlv8.js';
 dotenv.config(); // Load environment variables from .env file
 const { DB_SERVER, DB_DATABASE, DB_DRIVER } = process.env;
 
-/**
+/** Windows Authentication
  * Database configuration for SQL Server using mssql/msnodesqlv8 driver.
  */
 export const dbConfig = {
@@ -18,6 +18,23 @@ export const dbConfig = {
     trustServerCertificate: true, // For development only
   },
 };
+
+/** SQL Server Authentication
+ * Database configuration for SQL Server using mssql/msnodesqlv8 driver.
+ */
+// export const dbConfig = {
+//   server: DB_SERVER,
+//   database: DB_DATABASE,
+//   driver: DB_DRIVER,
+//   username: DB_USERNAME,
+//   password: DB_PASSWORD,
+//   connectionString: `Server=${DB_SERVER};Database=${DB_DATABASE};User ID=${DB_USERNAME};Password=${DB_PASSWORD};Driver=${DB_DRIVER}`,
+//   connectionTimeout: 30000,
+//   options: {
+//     encrypt: true, // For SQL Server Azure
+//     trustServerCertificate: true, // For development only
+//   },
+// };
 
 /**
  * Establishes a connection to the database using the configured dbConfig.
